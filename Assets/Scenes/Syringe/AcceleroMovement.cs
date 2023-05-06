4bea9ee24d22b8d5a591b4378e7e4339f471c4e5
Freller01 <74055835+Seckit909@users.noreply.github.com>
2023-05-07 01:12:28 +0200

    Gamer 1

---
+++ Assets/Scenes/Syringe/AcceleroMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcceleroMovement : MonoBehaviour
+{
+    [SerializeField] public float Movespeed = 0.5f;
+    [SerializeField] public float maxMagnitude = 1.0f;
+    private void Update()
+    {
+        Vector2 direction = new Vector2(Input.acceleration.x, Input.acceleration.y);
+        float acceleration = Movespeed;
+
+        // Restrict movement to 2D space
+        direction = Vector2.ClampMagnitude(direction, maxMagnitude);
+
+        transform.Translate(direction * acceleration);
+    }
+}
